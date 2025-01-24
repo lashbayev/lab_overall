4f3404b716e2634b0e49d026b44c6edbec4441ed
lashbayev <nurbeklashbaevaz@gmail.com>
2025-01-24 07:42:43 +0500

    Add files via upload

--- w3.py
+++ w3.py
@@ -1,50 +1,22 @@
-#Functions
-def my_function():
-  print("Hello from a function")
+#Modules
+import mymodule
+import platform #built-in platform
 
-my_function()
+mymodule.greeting('Khaby')
 
-#The number of parameters of the function always coincides with the number of arhuments when call
-def my_function(fname, lname):
-  print(fname + " " + lname)
+a = mymodule.person1["age"]
+print(a)
 
-my_function("Emil", "Refsnes")
+x = platform.system()
+print(x) #outputs windows
 
-#Arbitrary arguments
-def my_function(*kids):
-  print("The youngest child is " + kids[2])
+x = dir(platform)
+print(x)
 
-my_function("Emil", "Tobias", "Linus")
-#kwargs:
-def my_function(child3, child2, child1):
-  print("The youngest child is " + child3)
+x = dir(mymodule)
+print(x)
 
-my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
-#arbitrary kwargs:
-def my_function(**kid):
-  print("His last name is " + kid["lname"])
+#'from' keyword
+from mymodule import person1
 
-my_function(fname = "Tobias", lname = "Refsnes")
-#Default parameter value
-def my_function(country = "Norway"):
-  print("I am from " + country)
-
-my_function("Sweden")
-my_function("India")
-my_function()
-my_function("Brazil")
-#Sending a list as an argument:
-def my_function(food):
-  for x in food:
-    print(x)
-
-fruits = ["apple", "banana", "cherry"]
-#Return values
-my_function(fruits)
-
-def my_function(x):
-  return 5 * x
-
-print(my_function(3))
-print(my_function(5))
-print(my_function(9))
+print (person1["age"])
